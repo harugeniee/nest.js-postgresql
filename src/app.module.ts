94339053a5c87c598a5258e9534336ed0c4186a2
Hung Nguyen <360anime.suki@gmail.com>
2025-07-28 23:00:32 +0700

    Add caching support with Keyv and Redis integration in app.module.ts
    - Introduced @keyv/redis and @nestjs/cache-manager dependencies in package.json for caching capabilities.
    - Updated yarn.lock to include new dependencies and their versions.
    - Configured CacheModule in app.module.ts to utilize Keyv with a memory store and Redis for caching, enhancing application performance.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
+import { createKeyv } from '@keyv/redis';
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheableMemory } from 'cacheable';
+import { Keyv } from 'keyv';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -16,6 +20,17 @@ import { AppService } from './app.service';
       // imports: [ConfigModule],
       // useClass: TypeOrmConfigService,
       // inject: [ConfigService],
+    }),
+    CacheModule.registerAsync({
+      useFactory: () => {
+        const keyvOptions: any = {
+          store: new CacheableMemory({ ttl: 60000, lruSize: 5000 }),
+        };
+
+        return {
+          stores: [new Keyv(keyvOptions), createKeyv('redis://localhost:6379')],
+        };
+      },
     }),
   ],
   controllers: [AppController],
