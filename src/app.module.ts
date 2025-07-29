18fa7896f6398459b1161a5237c0c1dbb9bca39e
Hung Nguyen <360anime.suki@gmail.com>
2025-07-29 22:38:02 +0700

    Add Users module with controller, service, and DTOs
    - Introduced UsersModule to manage user-related functionality.
    - Created UsersController for handling user requests and responses.
    - Implemented UsersService for business logic related to users.
    - Added CreateUserDto and UpdateUserDto for user data validation and transformation.
    - Included a basic User entity extending BaseEntityCustom for database representation.
    - Updated app.module.ts to import UsersModule, enhancing application structure.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from '@nestjs-modules/ioredis';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -25,6 +26,7 @@ import { RedisModule } from '@nestjs-modules/ioredis';
         url: configService.get<string>('REDIS_URL'),
       }),
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,7 @@
+import { BaseEntityCustom } from 'src/common/entities/base.entity';
+import { Column } from 'typeorm';
+
+export class User extends BaseEntityCustom {
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  name: string;
+}
