7643f24e05b0cf763f3361a954fff336731d7937
onepiecehung <360anime.suki@gmail.com>
2025-07-31 17:51:18 +0700

    Add data source configuration and migration scripts
    - Introduced data-source.ts for PostgreSQL database connection configuration using TypeORM and environment variables.
    - Updated package.json to include TypeORM CLI commands for migration generation, running, and reverting.
    - Added gen-migration.ts script to facilitate the creation of new migrations via user input.
    - Modified tsconfig.build.json to exclude the new data-source.ts and database-related directories from the build process.

---
+++ data-source.ts
@@ -0,0 +1,20 @@
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+
+config();
+
+const configService = new ConfigService();
+
+export default new DataSource({
+  type: 'postgres',
+  host: configService.get('DATABASE_HOST'),
+  port: Number(configService.get('DATABASE_PORT')),
+  username: configService.get('DATABASE_USER'),
+  password: configService.get('DATABASE_PASSWORD'),
+  database: configService.get('DATABASE_NAME'),
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/db/migrations/*{.ts,.js}'],
+  synchronize: false,
+  logging: true,
+});
